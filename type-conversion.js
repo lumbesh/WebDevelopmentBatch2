4b345ebb526a70a135b0b2f9395a189de462fc4d
lumbesh <aj.lumbes@gmail.com>
2021-02-08 04:47:33 +0800

    02-04-2021 Assignment Part 1 & 2 LO1

---
+++ type-conversion.js
@@ -0,0 +1,8 @@
+let val;
+
+//Number to string
+val = String(123);
+
+console.log(val);
+
+console.clear();
---
+++ variable.js
@@ -0,0 +1,25 @@
+var greeting;
+console.log(greeting);
+greeting = 'hola lods';
+
+var sir = ' john ';
+console.log(greeting + sir);
+console.log(sir + greeting);
+
+const person = {
+    name: 'sando',
+    age: 30
+
+};
+
+person.name= 'sara';
+
+console.log(person);
+
+const numbers = ['a', 'b', 'c'];
+numbers.push(6);
+console.log(numbers);
+
+var blk = 'jojo';
+console.log(blk);
+console.clear();
